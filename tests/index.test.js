bae111507b2456b4ac1df9927fea26f2f1c0e813
tugrulkarakaya1 <tugrulkarakaya00@gmail.com>
2025-09-18 14:31:15 +0300

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,1 +1,17 @@
+const { addTodo, listTodos, clearTodos } = require("../src/index");
+
+beforeEach(() => {
+  clearTodos();
+});
+
+test("todo ekleme", () => {
+  addTodo("Test yaz");
+  expect(listTodos()).toContain("Test yaz");
+});
+
+test("todo temizleme", () => {
+  addTodo("Silinecek");
+  clearTodos();
+  expect(listTodos()).toHaveLength(0);
+});
 
