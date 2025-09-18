d5828a776e65d8a546e4c3b0c227a82c55f01d9e
tugrulkarakaya1 <tugrulkarakaya00@gmail.com>
2025-09-18 14:31:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+let todos = [];
+
+function addTodo(task) {
+  todos.push(task);
+  return todos;
+}
+
+function listTodos() {
+  return todos;
+}
+
+function clearTodos() {
+  todos = [];
+  return todos;
+}
+
+module.exports = { addTodo, listTodos, clearTodos };
+
+if (require.main === module) {
+  console.log("Todo App Çalışıyor 🚀");
+  addTodo("Workflow hazırla");
+  console.log("Todos:", listTodos());
+}
 
