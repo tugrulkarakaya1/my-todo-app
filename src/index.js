4db6204526d7f3d7e25a7dd9a6ba82b7ef96aff5
tugrulkarakaya1 <tugrulkarakaya00@gmail.com>
2025-09-18 17:16:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 let todos = [];
 
+// ❌ Örnek gizli bilgi (dummy API key gibi)
+const API_SECRET_KEY = "mysecretapikey12345";
+const PASSWORD = "sifrem"
 function addTodo(task) {
   todos.push(task);
   return todos;
@@ -14,11 +17,13 @@ function clearTodos() {
   return todos;
 }
 
-module.exports = { addTodo, listTodos, clearTodos };
+module.exports = { addTodo, listTodos, clearTodos, API_SECRET_KEY };
 
 if (require.main === module) {
   console.log("Todo App Çalışıyor 🚀");
   addTodo("Workflow hazırla");
   console.log("Todos:", listTodos());
-}
 
+  // secret key konsola yazdırma (sadece test için!)
+  console.log("Secret Key:", API_SECRET_KEY);
+}
